578b35ec4060030ba89ec21f115516979a22d41b
Alexander Leonov <spawnleon@gmail.com>
2023-06-25 13:47:39 +0400

    refactor: refactor rl behavior

--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
 import readline from 'node:readline';
 import os from 'node:os';
+import { EXIT_MESSAGE } from './const.js';
 import getArguments from './modules/cli/args.js';
 import changeDirectory from './modules/file-system/change-directory.js';
-
 
 const args = getArguments();
 const username = args.username || 'Guest';
 
 console.log(`Welcome to the File Manager, ${username}!`);
 
-
 let currentDirectory = os.homedir();
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: `${currentDirectory}> `
+  prompt: `${currentDirectory}> `,
 });
 
 rl.prompt();
 
+process.on('SIGINT', () => {
+  console.log(EXIT_MESSAGE(username));
+  process.exit();
+});
+rl.on('close', () => {
+  console.log(EXIT_MESSAGE(username));
+  process.exit();
+});
+
 rl.on('line', async (input) => {
   const [command, ...args] = input.split(' ');
 
-  switch(command) {
+  switch (command) {
     case '.exit':
-      process.exit();
+      process.kill(process.pid, 'SIGINT');
       break;
     case 'cd':
       currentDirectory = await changeDirectory(args[0]);
---
+++ src/const.js
@@ -0,0 +1,7 @@
+export  const EXIT_MESSAGE =  (username) => {
+  return `Thank you for using File Manager, ${username}, goodbye!`
+}
+
+export  const GREAT_MESSAGE =  (username) => {
+  return `Welcome to the File Manager, ${username}!`
+}
